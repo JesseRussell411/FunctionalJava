eac9f6fb87d5e9566f4906d2aa5b96cf84b63cd1
Jesse Russell <jesserussell411@gmail.com>
2022-03-15 10:40:33 -0600

    added 2 arg memo function

---
+++ src/memoization/MemoizedBiFunction.java
@@ -0,0 +1,18 @@
+package memoization;
+
+import java.util.function.BiFunction;
+
+public class MemoizedBiFunction<T, U, R> implements BiFunction<T, U, R> {
+    private final MemoizedFunction<Arguments<T, U>, R> function;
+
+    public MemoizedBiFunction(BiFunction<T, U, R> original) {
+        function = new MemoizedFunction<>((args) -> original.apply(args.t, args.u));
+    }
+
+    public R apply(T t, U u) {
+        return function.apply(new Arguments<>(t, u));
+    }
+
+    private record Arguments<T, U>(T t, U u) {
+    }
+}
