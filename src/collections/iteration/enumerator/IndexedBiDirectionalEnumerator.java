ea52bb8ea1441f17a6ae3b3d5d35a81384763111
Jesse Russell <jesserussell411@gmail.com>
2022-10-10 11:59:37 -0600

    optimized imports

--- src/collections/iteration/enumerator/IndexedBiDirectionalEnumerator.java
+++ src/collections/iteration/enumerator/IndexedBiDirectionalEnumerator.java
@@ -1,7 +1,4 @@
 package collections.iteration.enumerator;
 
-import collections.iteration.enumerator.BiDirectionalEnumerator;
-import collections.iteration.enumerator.IndexedEnumerator;
-
 public interface IndexedBiDirectionalEnumerator<T> extends IndexedEnumerator<T>, BiDirectionalEnumerator<T> {
 }
